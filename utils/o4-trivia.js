ea5838a86bacb65af9e7ab0ef1570e2b1d010e69
codignMustache <jorge@codingMustache.dev>
2025-09-05 21:51:52 -0700

    added randomising on the array

--- utils/o4-trivia.js
+++ utils/o4-trivia.js
@@ -4,6 +4,7 @@ const res = await fetch(
 const json = await res.json();
 const { question, incorrect_answers, correct_answer } = json.results[0];
 const ansArr = [...incorrect_answers, correct_answer]
+    .sort(() => Math.random() - 0.5)
     .map((e, i) => ` ${i + 1}. ${e}\n`)
     .join("");
 export default `
