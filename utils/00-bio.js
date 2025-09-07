293021046f005b55a9364ab44f832868dea516c0
codignMustache <jorge@codingMustache.dev>
2025-09-07 10:22:08 -0700

    lgtm?

--- utils/00-bio.js
+++ utils/00-bio.js
@@ -1,1 +1,8 @@
 export default `## Bio\n\nHello I'm Jorge and I love making things for the internet`;
+
+
+
+sudo wget -O /tmp/gitea https://dl.gitea.io/gitea/1.24.5/gitea-1.24.5-linux-amd64
+
+
+https://dl.gitea.com/gitea/1.24.5/gitea-1.24.5-linux-arm64?_gl=1*136ofmo*_gcl_au*NjcwNDczODA4LjE3NTQ5NDYyMTQ.
